4593b87304c862e700f3f0335d97e1735b5060ad
Boaz Lavon <boazlavon@gmail.com>
2021-10-17 08:48:40 -0700

    try except wrap

--- src/main.cc
+++ src/main.cc
@@ -51,12 +51,11 @@ int main(
     return ERROR_INPUT_PARAMS;
   }
 
-  string secured_hosts_path(argv[ARGV_SECURED_HOSTS_PATH]);
-  string common_passwords_path(argv[ARGV_PASSWORDS_PATH]);
-  string json_captures_path(argv[ARGV_JSON_CAPTURES]);
-  DetectionManager manager(secured_hosts_path, common_passwords_path, json_captures_path);
-
   try {
+    string secured_hosts_path(argv[ARGV_SECURED_HOSTS_PATH]);
+    string common_passwords_path(argv[ARGV_PASSWORDS_PATH]);
+    string json_captures_path(argv[ARGV_JSON_CAPTURES]);
+    DetectionManager manager(secured_hosts_path, common_passwords_path, json_captures_path);
     manager.execute();
   } catch (...) {
     result = RESULT_FAILURE;
